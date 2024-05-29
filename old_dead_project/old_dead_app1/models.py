61c0e621daec466509eb5fbaa31567b95a26bcee
Andrey Derevyanko <andrey.v.derevyanko@outlook.com>
2024-05-29 22:11:06 +0300

    New class

--- old_dead_project/old_dead_app1/models.py
+++ old_dead_project/old_dead_app1/models.py
@@ -33,5 +33,8 @@ class Country(models.Model):
     name = models.CharField(max_length=100, blank=False)
     flag = models.ImageField(width_field=50, height_field=50)
 
+    class Meta:
+        verbose_name_plural = "Countries"
+
     def __str__(self):
         return f'{self.name} {self.flag}'
