d9bb4c44217bba4e2aa652651f14aee4ce4d9cfa
Andrey Derevyanko <andrey.v.derevyanko@outlook.com>
2024-06-02 12:46:44 +0300

    Seems everything works

--- old_dead_project/old_dead_app1/admin.py
+++ old_dead_project/old_dead_app1/admin.py
@@ -4,6 +4,14 @@ from django.utils.html import format_html
 
 
 class CountryAdmin(admin.ModelAdmin):
+    """Здесь мы делаем так, чтобы не ссылки на флаги, а сами флаги (изображения из директории ./media/flags)
+    выводились рядом со странами в админке
+    Объяснение кода
+    Метод flag_thumbnail: Этот метод используется для создания HTML-кода, который отображает миниатюру флага.
+    Метод format_html безопасно форматирует строку как HTML.
+    list_display: Добавляет flag_thumbnail к списку полей, отображаемых в админке.
+    short_description: Устанавливает заголовок колонки в админке."""
+
     list_display = ('name', 'flag_thumbnail')
 
     def flag_thumbnail(self, obj):
