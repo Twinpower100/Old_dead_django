c99b639faabf07914d91132c1bb19552fb735a4f
Andrey Derevyanko <andrey.v.derevyanko@outlook.com>
2024-06-05 00:00:05 +0300

    Add Student, Group, Subject, and HoursNumber models
    Three new models, Student, Group, and Subject, have been added for managing student group data. A model named HoursNumber has also been introduced to keep track of the hours associated with each subject for each group. A minor formatting adjustment was made to Teacher model's currency related field.

--- old_dead_project/old_dead_app1/models.py
+++ old_dead_project/old_dead_app1/models.py
@@ -79,12 +79,52 @@ class Teacher(models.Model):
     teaching_degree = models.ForeignKey('TeachingDegree', blank=False, on_delete=models.CASCADE)
     external_teacher = models.BooleanField(default=False)
     salary = models.IntegerField(blank=False, null=False, serialize=True)
-    currency_abbreviation = models.ForeignKey('Currency', to_field='abbreviation', blank=False, on_delete=models.PROTECT)
+    currency_abbreviation = models.ForeignKey('Currency', to_field='abbreviation',
+                                              blank=False, on_delete=models.PROTECT)
     capacity = models.IntegerField(verbose_name='Capacity in hours per week')
 
     def __str__(self):
         return (
                 f'{self.name} {self.surname} {self.citizenship} {self.science_degree}'
                 f'{self.teaching_degree} {self.external_teacher} {self.salary}'
-                f' {self.currency_abbreviation} {self.capacity}'
-                )   
+                f'{self.currency_abbreviation} {self.capacity}'
+                )
+
+
+class Student(models.Model):
+    objects = None
+    name = models.CharField(max_length=40, blank=False)
+    surname = models.CharField(max_length=100, blank=False)
+    citizenship = models.ForeignKey(Country, on_delete=models.CASCADE)
+    group = models.ForeignKey('Group', to_field='speciality', blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} {self.surname} {self.citizenship} {self.group}'
+
+
+class Group(models.Model):
+    objects = None
+    number = models.IntegerField(unique=True, blank=False)
+    speciality = models.CharField(max_length=50, blank=False)
+    head = models.ForeignKey('Student', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.number} {self.speciality} {self.head}'
+
+
+class Subgect(models.Model):
+    objects = None
+    name = models.CharField(max_length=150, blank=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class HoursNumber(models.Model):
+    objects = None
+    group = models.ForeignKey('Group', to_field='number', blank=False, on_delete=models.CASCADE)
+    subject = models.ForeignKey('Subgect', to_field='name', blank=False, on_delete=models.CASCADE)
+    number_of_hours = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return f'{self.group} {self.subject} {self.number_of_hours}'
